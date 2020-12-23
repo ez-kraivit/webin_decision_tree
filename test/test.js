d03852324cb2b464644427e0ceccd08de4deb4d1
Kraivit Mongkhonsakunrit <kraivit.m@edvisory.co.th>
2020-12-23 22:44:53 +0700

    24

--- test/test.js
+++ test/test.js
@@ -1,17 +1,18 @@
 // const demo_json = require('../example/demo.json')
-const assert = require('assert');
-const decision_tree = require('lib/decision_tree');
+const assert = require('assert'),decision_tree = require('lib/decision_tree');
 describe('Decision Tree By Kraivit', () => {
     describe('#DEMO', () => {
         context('ทดสอบการส่งพารามิเตอร์ในตัวแปร ต่าง ๆ', function () {
             it('String', function () {
                 assert(decision_tree.wordcut("กลัว กลัว ทำไมละนั้น กลัวจังเลย"))
+                // decision_tree.wordcut("กลัว กลัว ทำไมละนั้น กลัวจังเลย").then(console.log)
             });
             it('Array', function () {
                 assert(decision_tree.wordcut(["กลัว กลัว ทำไมละนั้น กลัวจังเลย", "สวัสดีครับ"]))
             });
             it('NuN', function () {
                 assert(decision_tree.wordcut(NaN))
+                // console.log(decision_tree.wordcut(NaN))
             });
             it('Null', function () {
                 assert(decision_tree.wordcut(null))
